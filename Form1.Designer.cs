771e8ff257b0ffd43f451efaa5e3223f8ee5c2a1
Knock <hr@gmail>
2026-02-28 00:06:23 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Lab_2
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ListBox taskList;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Label resultLabel;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.taskList = new System.Windows.Forms.ListBox();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // taskList
+            // 
+            this.taskList.FormattingEnabled = true;
+            this.taskList.ItemHeight = 16;
+            this.taskList.Items.AddRange(new object[] {
+            "1. Сумма 10 чисел",
+            "2. Макс и мин в массиве",
+            "3. Четные и нечетные",
+            "4. Реверс массива",
+            "5. Сдвиг вправо",
+            "6. Подсчет повторений"});
+            this.taskList.Location = new System.Drawing.Point(12, 12);
+            this.taskList.Name = "taskList";
+            this.taskList.Size = new System.Drawing.Size(200, 276);
+            this.taskList.TabIndex = 0;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(230, 12);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(340, 100);
+            this.inputBox.TabIndex = 1;
+            // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(230, 118);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(150, 30);
+            this.runButton.TabIndex = 2;
+            this.runButton.Text = "Выполнить";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.BackColor = System.Drawing.Color.White;
+            this.resultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.resultLabel.Location = new System.Drawing.Point(230, 154);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(340, 134);
+            this.resultLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 303);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.taskList);
+            this.Name = "Form1";
+            this.Text = "Задачи с массивами";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int task = taskList.SelectedIndex;
+                if (task < 0)
+                {
+                    resultLabel.Text = "Выберите задачу!";
+                    return;
+                }
+
+                switch (task)
+                {
+                    case 0: Task1(); break;
+                    case 1: Task2(); break;
+                    case 2: Task3(); break;
+                    case 3: Task4(); break;
+                    case 4: Task5(); break;
+                    case 5: Task6(); break;
+                }
+            }
+            catch (Exception ex)
+            {
+                resultLabel.Text = "Ошибка: " + ex.Message;
+            }
+        }
+
+        // Задача 1: Сумма 10 чисел
+        private void Task1()
+        {
+            int[] arr = GetNumbers(10);
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+                sum += arr[i];
+
+            resultLabel.Text = $"Сумма = {sum}";
+        }
+
+        // Задача 2: Максимум и минимум
+        private void Task2()
+        {
+            if (!int.TryParse(inputBox.Text, out int n) || n <= 0)
+            {
+                resultLabel.Text = "Введите положительное число!";
+                return;
+            }
+
+            Random rnd = new Random();
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+                arr[i] = rnd.Next(1, 101);
+
+            int max = arr[0], min = arr[0];
+            int imax = 0, imin = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[i] > max) { max = arr[i]; imax = i; }
+                if (arr[i] < min) { min = arr[i]; imin = i; }
+            }
+
+            resultLabel.Text = $"Массив: {string.Join(" ", arr)}\n" +
+                              $"Макс: {max}[{imax}], Мин: {min}[{imin}]";
+        }
+
+        // Задача 3: Четные и нечетные
+        private void Task3()
+        {
+            Random rnd = new Random();
+            int[] arr = new int[15];
+            int even = 0, odd = 0;
+
+            for (int i = 0; i < 15; i++)
+                arr[i] = rnd.Next(10, 51);
+
+            foreach (int x in arr)
+            {
+                if (x % 2 == 0) even++;
+                else odd++;
+            }
+
+            resultLabel.Text = $"Массив: {string.Join(" ", arr)}\n" +
+                              $"Четных: {even}, Нечетных: {odd}";
+        }
+
+        // Задача 4: Реверс массива
+        private void Task4()
+        {
+            int[] arr = GetNumbers();
+            int[] rev = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+                rev[i] = arr[arr.Length - 1 - i];
+
+            resultLabel.Text = $"Было: {string.Join(" ", arr)}\n" +
+                              $"Стало: {string.Join(" ", rev)}";
+        }
+
+        // Задача 5: Сдвиг вправо
+        private void Task5()
+        {
+            int[] arr = GetNumbers();
+            int last = arr[arr.Length - 1];
+
+            for (int i = arr.Length - 1; i > 0; i--)
+                arr[i] = arr[i - 1];
+            arr[0] = last;
+
+            resultLabel.Text = $"Результат: {string.Join(" ", arr)}";
+        }
+
+        // Задача 6: Подсчет повторений
+        private void Task6()
+        {
+            int[] arr = GetNumbers();
+            bool[] counted = new bool[arr.Length];
+            string result = "";
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (counted[i]) continue;
+
+                int count = 1;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] == arr[i])
+                    {
+                        count++;
+                        counted[j] = true;
+                    }
+                }
+                result += $"Число {arr[i]} - {count} раз(а)\n";
+            }
+            resultLabel.Text = result;
+        }
+
+        // Вспомогательный метод для ввода чисел
+        private int[] GetNumbers(int needCount = 0)
+        {
+            string text = inputBox.Text.Trim();
+            if (string.IsNullOrEmpty(text))
+                throw new Exception("Введите числа!");
+
+            string[] parts = text.Split(new char[] { ' ', ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] nums = new int[parts.Length];
+
+            for (int i = 0; i < parts.Length; i++)
+                nums[i] = int.Parse(parts[i]);
+
+            if (needCount > 0 && nums.Length != needCount)
+                throw new Exception($"Нужно {needCount} чисел!");
+
+            return nums;
+        }
+    }
+}
